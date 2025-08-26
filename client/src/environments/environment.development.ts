b11a6416a06a756aeb6819f88ef2b9cddbd9fc4c
manouch <manoujon@gmail.com>
2025-08-26 14:08:24 -0500

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api/'
+  apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey: 'pk_test_51RxSir8NtDULzS2J4l7E7YmvP7OGof07FGgrm6Daypk6xHiJQBxX1ltSuPigoEW0WVMlutHhNn9eNlffr71xjgaD00SkyGWMYn'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'api/',
+  stripePublicKey: 'pk_test_51RxSir8NtDULzS2J4l7E7YmvP7OGof07FGgrm6Daypk6xHiJQBxX1ltSuPigoEW0WVMlutHhNn9eNlffr71xjgaD00SkyGWMYn'
 };
